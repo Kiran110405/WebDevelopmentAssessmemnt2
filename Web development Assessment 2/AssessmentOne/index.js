22293babaae3099e6cc25003d798373714247ee8
Kiran110405 <147168552+Kiran110405@users.noreply.github.com>
2025-11-29 21:25:09 +0000

    register progress

--- Web development Assessment 2/AssessmentOne/index.js
+++ Web development Assessment 2/AssessmentOne/index.js
@@ -2,6 +2,9 @@ const express = require("express"); //declares content of express module of expr
 const app = express(); //executes the function
 const path = require("path");
 
+//const posts = require("./models/posts");
+const users = require("./models/users");
+
 const userModel = require("./models/users");
 
 app.use(express.static("public"));
@@ -15,17 +18,36 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/map", (request, response) => {
-  response.sendFile(path.join(__dirname, "/views", "map.html"));
-});
-
-app.get("/register", (request, response) => {
-  response.sendFile(path.join(__dirname, "/views", "register.html"));
-});
-
-app.get("/notes", (request, response) => {
-  response.sendFile(path.join(__dirname, "/views", "notes.html"));
+  response.sendFile(path.join(__dirname, "/public", "map.html"));
 });
 
 app.get("/login", (request, response) => {
-  response.sendFile(path.join(__dirname, "/views", "login.html"));
+  response.sendFile(path.join(__dirname, "/public", "login.html"));
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (users.checkUser(username, password)) {
+    return res.sendFile(path.join(__dirname, "/views", "notes.html"));
+  }
+
+  return res.sendFile(path.join(__dirname, "/views", "login_failed.html"));
+});
+
+app.get("/register", (request, response) => {
+  response.sendFile(path.join(__dirname, "/public", "register.html"));
+});
+
+app.post("/register", (request, response) => {
+  if (users.addUser(request.body.username, request.body.password)) {
+    response.sendFile(path.join(__dirname, "/public", "login.html"));
+  }
+  response.sendFile(
+    path.join(__dirname, "/public", "registration_failed.html")
+  );
+});
+
+app.get("/notes", (request, response) => {
+  response.sendFile(path.join(__dirname, "/public", "notes.html"));
 });
--- Web development Assessment 2/AssessmentOne/models/users.js
+++ Web development Assessment 2/AssessmentOne/models/users.js
@@ -1,44 +1,39 @@
 let users = [
-  //users that are already stored in the database
-  {
-    username: "user1",
-    password: "123",
-  },
-  {
-    username: "user2",
-    password: "456",
-  },
+  { username: "user1", password: "123" },
+  { username: "user2", password: "456" },
 ];
 
 function addUser(username, password) {
   let existingUser = findUser(username);
-  if (!exisitinguser) {
+  console.log(existingUser);
+  if (!existingUser) {
     let newUser = {
       username: username,
       password: password,
     };
+
     users.push(newUser);
     return true;
   }
+
+  return false; // user already exists
 }
-return false;
 
 function checkUser(username, password) {
-  let foundUser = findUser(username); //check if username exists in database of users
+  let foundUser = findUser(username);
   if (foundUser) {
-    return foundUser.password == password; //checks that the user matches the password
+    return foundUser.password == password;
   }
-  return false; //If doesnt match return as false
+  return false;
 }
 
 function findUser(username) {
   return users.find((thisUser) => thisUser.username == username);
 }
 
-function getusers() {}
-
-module.export = {
+module.exports = {
   addUser,
   checkUser,
   findUser,
+  users,
 };
